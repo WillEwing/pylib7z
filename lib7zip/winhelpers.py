af72c51e7815b758c3f451f92fe0feacf8f34990
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-21 04:59:44 -0400

    Switched back to flat layout.

---
+++ lib7zip/winhelpers.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Helper functions for dealing with windows types defined in wintypes like PROPVARIANT, GUID*, and HRESULT
+"""
+
+import uuid
+
+# from bitstring import BitArray
+# import warnings
+from datetime import datetime, timedelta
+
+from . import CreatePropVariant, ffi, log
+from .ffi7z_types import *
+
+
+class HRESULTException(Exception):
+    pass
+
+
+def RNOK(code):
+    """raise error if not S_OK"""
+    # TODO raise different Exception based on result
+    if code == HRESULT.S_OK.value:
+        return
+    # parse HRESULT
+
+    """
+	barr = BitArray(uint=code, length=32)
+
+	# first 2 bits are status
+	SUCCESS = '0b00'
+	INFO = '0b01'
+	WARN = '0b10'
+	ERROR = '0b11'
+	status = barr[0:2]
+
+	#next says whether this is user-defined or not
+	is_cust = barr[2]
+	# reserved bit (should always be 0?)
+	reserved = barr[3]
+	log.debug('is_cust=%r; reserved=%r', is_cust, reserved)
+	# what errored e.g. the "facility"
+	facility = barr[4:12].uint
+	# finally, the code
+	h_code = barr[16:].uint
+
+	if code == HRESULT.S_FALSE:
+		raise HRESULTException('S_FALSE')
+	elif status == SUCCESS:
+		log.info('SUCCESS, but not S_OK, %d/%04x', facility, h_code)
+	elif status == INFO:
+		log.info('INFO %d/%04x', facility, h_code)
+	elif status == WARN:
+		log.warn('WARNING, %d/%04x' % (facility, h_code))
+	elif status == ERROR:
+		try:
+			hresult = HRESULT(code)
+			raise HRESULTException(hresult.name + ': ' + hresult.desc)
+		except ValueError:
+			raise HRESULTException('HRESULT, %d/04x', facility, h_code)
+	"""
+
+    try:
+        hresult = HRESULT(code)
+        raise HRESULTException(hresult.name + ": " + hresult.desc)
+    except ValueError:
+        raise HRESULTException("HRESULT, %08x", code)
+
+
+def dealloc_propvariant(pvar):
+    pass
+
+
+def alloc_propvariant():
+    return CreatePropVariant()
+
+
+def get_prop_val(fn, forcetype=None, checktype=None):
+    """
+    fn should have the signature:
+    HRESULT fn(PROPVARIANT*);
+    """
+    ptr = alloc_propvariant()
+    RNOK(fn(ptr))
+    pvar = ffi.cast("PROPVARIANT*", ptr)
+    vt = forcetype or VARTYPE(pvar.vt)
+    if pvar.vt in (VARTYPE.VT_EMPTY, VARTYPE.VT_NULL):
+        # always check for null, pvar.vt is not a bug
+        return None
+
+    if checktype:
+        if checktype == True:
+            checktype = forcetype
+        assert pvar.vt == checktype
+
+    if vt in (VARTYPE.VT_UI4, VARTYPE.VT_UINT):
+        return int(pvar.ulVal)
+    elif vt == VARTYPE.VT_UI8:
+        return int(pvar.uhVal)
+    elif vt == VARTYPE.VT_BOOL:
+        return int(pvar.bVal) != 0
+    elif vt == VARTYPE.VT_BSTR:
+        return ffi.string(pvar.bstrVal)
+    elif False:  # vt == VARTYPE.VT_FILETIME:
+        # FIXME This should work but it totally doesn't
+        timestamp = int(pvar.filetime.dwLowDateTime)
+        timestamp += int(pvar.filetime.dwLowDateTime) << 32
+        # timestamp is in 100-nanosecond intervals, convert to nanoseconds
+        timestamp *= 100
+        # convert to seconds
+        timestamp /= 1e9
+
+        # timestamp is now the number of seconds since Jan 1, 1601 CE
+        jan01_1601 = datetime(year=1601, month=1, day=1)
+        delta = timedelta(seconds=timestamp)
+        import pdb
+
+        pdb.set_trace()
+        return jan01_1601 + delta
+
+        # timestamp isn't guaranteed to be UTC (it isn't on FAT for example)
+        # it is however recommended, hope 7zip enforces that.
+    else:
+        raise TypeError("type code %d not supported" % vt)
