db3d9f45e0dcc8867b3e89100fc728210d34089e
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-21 05:11:57 -0400

    Got the build working. In tox, even.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
-from setuptools import setup
+import os.path
+import sys
+
+import setuptools
+
+sys.path.append(os.path.dirname(__file__))
 
 from ffi7zip.build_ffi7zip import ffibuilder
 
-setup(ext_modules=[(ffibuilder.distutils_extension())])
+setuptools.setup(ext_modules=[(ffibuilder.distutils_extension())])
