bb4307dd25873aea2c1076272f5ffd899d5a3354
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-21 10:59:06 -0400

    Integrated thunks.py generation into setup.py

--- ffi7zip/build_ffi7zip.py
+++ ffi7zip/build_ffi7zip.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3.8
 # -*- coding: utf-8 -*-
 
+import os.path
 from io import StringIO
+from typing import Dict, List, Optional
 
 from cffi import FFI
+from setuptools import Command
 
-from .idlgen import append_cdefs, append_cimpl
+from .idlgen import append_cdefs, append_cimpl, append_thunk_pyimpls
 
 
 def get_cimpl() -> str:
@@ -30,3 +33,29 @@ ffibuilder = FFI()
 ffibuilder.set_unicode(True)
 ffibuilder.set_source("lib7zip.ffi7zip", get_cimpl(), libraries=["Ole32"])
 ffibuilder.cdef(get_cdefs())
+
+
+class UpdateThunks(Command):
+    """
+    Subcommand for setuptools to update thunks from IDL.
+    """
+
+    build_lib: Optional[str]
+    editable_mode: bool
+
+    def initialize_options(self) -> None:
+        """Initialize options?"""
+        self.build_lib = None
+        self.editable_mode = False
+
+    def finalize_options(self) -> None:
+        """Finalize options?"""
+        self.set_undefined_options("build_py", ("build_lib", "build_lib"))
+
+    def run(self) -> None:
+        """Generated updated thunks file."""
+        thunks_path = "lib7zip/thunks.py"
+        with open(thunks_path, "w", encoding="utf-8") as thunks_py:
+            thunks_py.write("#!/usr/bin/env python\n# -*- coding: utf-8 -*-\n\n")
+            thunks_py.write("from ffi7zip import lib, ffi\n\n")
+            append_thunk_pyimpls(thunks_py)
--- setup.py
+++ setup.py
@@ -4,9 +4,23 @@ import os.path
 import sys
 
 import setuptools
+import setuptools.command.build
 
+# Latest setuptools/pip removes '' from sys.path, but it's needed here for code generation.
 sys.path.append(os.path.dirname(__file__))
+from ffi7zip.build_ffi7zip import (  # noqa, pylint: disable=wrong-import-position
+    UpdateThunks,
+    ffibuilder,
+)
 
-from ffi7zip.build_ffi7zip import ffibuilder
+setuptools.command.build.build.sub_commands.append(("update_thunks", None))
 
-setuptools.setup(ext_modules=[(ffibuilder.distutils_extension())])
+# Configure and run setuptools.
+setuptools.setup(
+    ext_modules=[
+        ffibuilder.distutils_extension(),  # type: ignore
+    ],
+    cmdclass={
+        "update_thunks": UpdateThunks,
+    },
+)
