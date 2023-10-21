31b0b3c535ce82c14c313b77caf9eba1360da453
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-21 06:40:36 -0400

    Cleaned up build_ffi7zip.py

--- ffi7zip/build_ffi7zip.py
+++ ffi7zip/build_ffi7zip.py
@@ -9,12 +9,18 @@ from .idlgen import append_cdefs, append_cimpl
 
 
 def get_cimpl() -> str:
+    """
+    Get C implementation source.
+    """
     stream = StringIO()
     append_cimpl(stream)
     return stream.getvalue()
 
 
 def get_cdefs() -> str:
+    """
+    Get IDL C definitions.
+    """
     stream = StringIO()
     append_cdefs(stream)
     return stream.getvalue()
@@ -24,11 +30,3 @@ ffibuilder = FFI()
 ffibuilder.set_unicode(True)
 ffibuilder.set_source("lib7zip.ffi7zip", get_cimpl(), libraries=["Ole32"])
 ffibuilder.cdef(get_cdefs())
-
-
-def main():
-    ffibuilder.compile(verbose=True)
-
-
-if __name__ == "__main__":
-    main()
