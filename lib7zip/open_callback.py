7355875fbc8458d58a39f101dd85fe3c6299ffdf
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-21 14:03:24 -0400

    Converted ArchiveOpenCallback to PyUnknown.

--- lib7zip/open_callback.py
+++ lib7zip/open_callback.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .ffi7z import *
+
+"""
+Python bindings for the 7-Zip Library: ArchiveOpenCallbacks
+"""
+
+from .ffi7zip import ffi  # pylint: disable=no-name-in-module
+from .hresult import HRESULT
+from .iids import (
+    IID_IArchiveOpenCallback,
+    IID_IArchiveOpenSetSubArchiveName,
+    IID_IArchiveOpenVolumeCallback,
+    IID_ICryptoGetTextPassword,
+)
+from .propvariant import VARTYPE
+from .unknown import PyUnknown
 
 
-class ArchiveOpenCallback(IUnknownImpl):
+class ArchiveOpenCallback(PyUnknown):
     """
-    Archive open callback implementation.
+    Archive Open Callbacks
     """
+
+    # pylint: disable=invalid-name
 
     IIDS = (
         IID_ICryptoGetTextPassword,
@@ -15,29 +31,29 @@ class ArchiveOpenCallback(IUnknownImpl):
         IID_IArchiveOpenSetSubArchiveName,
     )
 
-    def __init__(self, password=None, stream=None):
+    def __int__(self, password=None, stream=None):
         self.password = password
-        self.password_buf = ffi.new("wchar_t[]", password) if password else ffi.NULL
+        self.password_buf = ffi.new("wchar_t []", password) if password else ffi.NULL
         self.stream = stream
         self.subarchive_name = None
         super().__init__()
 
     def SetTotal(self, files, bytes):
-        return HRESULT.S_OK.value
+        return HRESULT.S_OK
 
     def SetCompleted(self, files, bytes):
-        return HRESULT.S_OK.value
+        return HRESULT.S_OK
 
     def CryptoGetTextPassword(self, password_ptr):
-        password[0] = self.password_buf
-        return RESULT.S_OK.value
+        password_ptr[0] = self.password_buf
+        return HRESULT.S_OK
 
     def GetProperty(self, prop_id, value):
         value.vt = VARTYPE.VT_EMPTY
-        return HRESULT.S_OK.value
+        return HRESULT.S_OK
 
     def GetStream(self, name, in_stream):
-        return HRESULT.E_NOTIMPL.value
+        return HRESULT.E_NOTIMPL
 
     def SetSubArchiveName(self, name):
-        return HRESULT.E_NOTIMPL.value
+        return HRESULT.E_NOTIMPL
