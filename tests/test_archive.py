2dce0850d2981b2ce0b4687be9762ef89d8128bb
William Ewing <will.ewing.iv+git@gmail.com>
2023-10-22 08:34:38 -0400

    Unskipped formerly-crashing tests.

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -3,6 +3,7 @@ import io
 import logging
 import os
 import shutil
+import sys
 from collections import namedtuple
 from functools import partial
 from glob import glob
@@ -10,18 +11,11 @@ from tempfile import mkdtemp
 
 import pytest
 
-from lib7zip import *
+from lib7zip import Archive, ArchiveItem
 
 log = logging.getLogger("lib7zip")
 
-simple_archives = ("tests/simple.7z", "tests/simple.zip")
-
-
-@pytest.fixture(scope="session")
-def tmp_dir(request):
-    path = mkdtemp()
-    request.addfinalizer(partial(shutil.rmtree, path))
-    return path
+SIMPLE_ARCHIVES = ("tests/simple.7z", "tests/simple.zip")
 
 
 _IX = namedtuple("ArchiveItemEx", ("is_dir", "crc", "contents"))
@@ -65,61 +59,65 @@ def test_complex():
                 assert item.crc == md.crc
 
             if not item.is_dir:
-                assert item.contents.decode("utf-8") == md.contents
+                assert item.read_text() == md.contents
         logging.debug("done iterating archives")
 
 
-def test_extract_dir_complex(tmp_dir):
+def test_extract_dir_complex(tmpdir):
     with Archive("tests/complex.7z") as archive:
-        archive.extract(tmp_dir)
+        archive.extract(tmpdir)
 
     for path, md in COMPLEX_MD.items():
         if md.contents:
-            with open(os.path.join(tmp_dir, path), encoding="utf-8") as f:
+            with open(os.path.join(tmpdir, path), encoding="utf-8") as f:
                 file_contents = f.read()
                 # if 'unicode' in path:
                 # 	import pdb; pdb.set_trace()
                 assert file_contents == md.contents
 
 
-@pytest.mark.parametrize("path", simple_archives)
+@pytest.mark.parametrize("path", SIMPLE_ARCHIVES)
 def test_extract_stream(path):
     with Archive(path) as archive:
-        stream = io.BytesIO()
-        archive[0].extract(stream)
-        assert stream.getvalue() == b"Hello World!\n"
+        assert archive[0].read_text() == "Hello World!\n"
 
 
-@pytest.mark.parametrize("path", simple_archives)
-def test_extract_dir(path, tmp_dir):
+@pytest.mark.parametrize("path", SIMPLE_ARCHIVES)
+def test_extract_dir(path, tmpdir):
     with Archive(path) as archive:
-        archive.extract(tmp_dir)
+        archive.extract(tmpdir)
 
-    with open(os.path.join(tmp_dir, "hello.txt"), "rb") as f:
+    with open(os.path.join(tmpdir, "hello.txt"), "rb") as f:
         assert f.read() == b"Hello World!\n"
 
 
-@pytest.mark.xfail(run=False)
+@pytest.mark.xfail()
 def test_extract_with_pass():
     with Archive("tests/simple_crypt.7z") as archive:
-        stream = io.BytesIO()
         assert archive[0].path == "hello.txt"
-        archive[0].extract(stream, password="password")
-        assert stream.getvalue() == b"Hello World!\n"
+        archive[0].read_text(password="password") == "Hello World!\n"
 
 
-@pytest.mark.xfail(run=False)
-def test_extract_with_pass_dir(tmp_dir):
-    with Archive("tests/simple crypt.7z", password="password") as archive:
-        archive.extract(tmp_dir)
+@pytest.mark.xfail()
+def test_extract_with_pass_dir(tmdir):
+    with Archive("tests/simple_crypt.7z", password="password") as archive:
+        archive.extract(tmdir)
 
-    with open(os.path.join(tmp_dir, "hello.txt"), "rb") as f:
+    with open(os.path.join(tmdir, "hello.txt"), "rb") as f:
         assert f.read() == b"Hello World!\n"
 
 
-@pytest.mark.xfail(run=False)
+@pytest.mark.xfail(raises=RuntimeError)
 def test_extract_badpass():
-    with Archive("tests/simple crypt.7z") as archive:
-        stream = io.BytesIO()
-        with pytest.raises(Exception):  # TODO catch correct exception
-            archive[0].extract(stream, password="notthepass")
+    with Archive("tests/simple_crypt.7z") as archive:
+        archive[0].read_bytes(password="notthepass")
+
+
+def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logging.getLogger("ffi7zip.thunks").info("Hello?")
+    test_extract_badpass()
+
+
+if __name__ == "__main__":
+    main()
